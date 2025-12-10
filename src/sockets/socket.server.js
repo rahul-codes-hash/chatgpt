86886b3a8d41d3149367acb6df019f8e16833139
rahul-codes-hash <rs10395@gmail.com>
2025-12-11 01:20:04 +0530

    Fourth Commit

--- src/sockets/socket.server.js
+++ src/sockets/socket.server.js
@@ -38,22 +38,46 @@ socket.on("ai-message" , async(messagePayload) =>{
     }
     */
     
-    // await messageModel.create({
-    //     chat: messagePayload.chat,
-    //     user: socket.user._id,
-    //     content: messagePayload.content,
-    //     role: 'user'
-    // })
+    // Step 1: Store user message in DB
+    const userMessage = await messageModel.create({
+        chat: messagePayload.chat,
+        user: socket.user._id,
+        content: messagePayload.content,
+        role: 'user'
+    })
 
+    // Step 2: Generate vector for user message
     const vectors = await aiService.generateVector(messagePayload.content);
 
-    console.log( "Vectors Generated" , vectors)
+    // Step 3: Query Pinecone for related Long Term Memories
+    //Long Term Memory, LTM are always stored in pinecone
+const memory = queryMemory({
+    queryVector: vectors,
+    limit: 3 ,
+    metadata: {
+        userId: socket.user._id,
+    }
+})
+
+console.log(memory)
+
+// Step 4: Save user message ( as memory ) in Pinecone
+// await createMemory({
+//     vector: vectors,
+//     metadata: {
+//         userId: socket.user._id.toString(),
+//         chatId: messagePayload.chat,
+//         content: messagePayload.content
+//     }
+// })
 
 
+
+    // Step 5: Get chat history ( last 20 messages ) from database 
     const chatHistory = (await messageModel.find({ chat: messagePayload.chat }).sort({ createdAt: - 1 }).limit(20).lean()).reverse();
 
-
-    const response = await aiService.generateResponse(chatHistory.map(item => { 
+    // Transform chat history to format suitable for AI service. Extract text parts from array of 20 messages in chatHistory
+    const stm = await aiService.generateResponse(chatHistory.map(item => { 
         return { 
             role: item.role, 
             parts: [{text : item.content}] 
@@ -61,15 +85,47 @@ socket.on("ai-message" , async(messagePayload) =>{
     }
     ))
 
+    // STM is generated from chat history retrieved from database (mongoDB or any other DB used by the application)
+    // LTM is generated from the memories queried / extracted from pinecone
+    const ltm = [
+        {
+            role: 'system',
+            parts: [{ text: `There are some previous messages from the chat , use them to generate a response 
+                ${memory.map(item => item.metadata.text).join('\n')}
+                ` }] 
+        }
+    ]
+
+    console.log(ltm[0])
+    console.log(stm)
+
+        // Step 6: Generate response from AI using chat history and LTM
+    const response = await aiService.generateResponse([...ltm , ...stm])
 
 
-        await messageModel.create({
+
+       const responseMessage = await messageModel.create({
         user: socket.user._id,
         chat: messagePayload.chat,
         content: response,
         role: 'model'
     })
 
+    // Step 7: Generate vector for AI response
+    const responseVectors = await aiService.generateVector(response);
+
+    // Step 8: Save AI response / memory ( as memory ) in Pinecone
+    // await createMemory({
+    //     vector: responseVectors,
+    //     metadata: {
+    //         userId: socket.user._id.toString(),
+    //         chatId: messagePayload.chat,
+    //         content: response
+    //     }
+    // })
+
+
+    // Step 9: Send AI response back to client / user
     socket.emit("ai-response" , {
         chatId: messagePayload.chat,
         content: response 
